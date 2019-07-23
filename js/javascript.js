6768d9a99031839d9b54e049855756bcf563f440
Adam <adam@adammarton.com>
2019-07-23 14:38:59 -0400

    meta tags and title

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,6 @@
+
+// Scripts for twitter and facebook button popups
+
     $(".icon-twitter").on("click", function(){
 
       var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
@@ -12,4 +15,14 @@
       var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";
     window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 
+    });
+
+
+
+// Jquery to make the text in the table of contents boxes appearon hover only
+
+    $(".hover-box").hover(function () {
+        $(".hover-text").addClass("display");
+    }, function () {
+        $(".hover-text").removeClass("display");
     });
